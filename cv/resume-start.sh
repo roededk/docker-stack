4cd33a9c2fcfe63b996a7aaffab301041ed193d8
Anders Jørgensen <aj@bsad.dk>
2020-05-21 16:38:45 +0000

    cv folder cleanup

--- cv/resume-start.sh
+++ cv/resume-start.sh
@@ -4,7 +4,3 @@
 /start.sh
 cp -R /usr/share/nginx/html/* /var/www/html/
 
-##Generate FR
-curl -o /usr/share/nginx/html/resume.json $RESUME_JSON_URL
-/start.sh
-cp /usr/share/nginx/html/index.html /var/www/html/fr.html
